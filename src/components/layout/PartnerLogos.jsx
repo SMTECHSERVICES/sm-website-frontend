56e9c3c2260951c8906fe560e75b7c3da9731dff
Er.Abhishek Mishra <abhishekmishra59@gmail.com>
2025-06-11 08:33:14 +0530

    Update section

--- src/components/layout/PartnerLogos.jsx
+++ src/components/layout/PartnerLogos.jsx
@@ -4,7 +4,7 @@ const PartnerLogos = () => {
     <div className="py-10 bg-gray-100 text-center">
       <h3 className="text-xl font-semibold text-gray-700 mb-6">Trusted By</h3>
       <div className="flex flex-wrap justify-center gap-6">
-        <img src="/logo1.png" alt="Client 1" className="h-12" />
+        <img src="/logo1.jpg" alt="Client 1" className="h-12" />
         <img src="/logo2.png" alt="Client 2" className="h-12" />
         <img src="/logo3.png" alt="Client 3" className="h-12" />
       </div>
